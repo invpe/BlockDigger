f0da0e1bc3925c8378b1ab14d2b8386269f77197
invpe <106522950+invpe@users.noreply.github.com>
2024-08-09 14:50:44 +0200

    Add files via upload

--- Client/main.cpp
+++ Client/main.cpp
@@ -12,7 +12,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <sstream>
-#define PICK_BUFFER_SIZE 32 * 32 * 32
+#define PICK_BUFFER_SIZE 16 * 16 * 16
 #define BLOCK_SCALE 0.5
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -23,7 +23,12 @@ const int BUFFER_SIZE = 2048;
 
 std::string receive_buffer;
 std::map<std::string,int> mPlayers;
-GLfloat fogColor[4] = {0.5f, 0.5f, 0.7f, 1.0f}; 
+GLfloat fogColor[4] = {0.15f, 0.15f, 0.165f, 1.0f}; 
+GLfloat lightPos[] = {0.0f, 10.0f, 10.0f, 1.0f};  // Position of light
+GLfloat lightAmbient[] = {0.2f, 0.2f, 0.2f, 1.0f}; // Ambient light
+GLfloat lightDiffuse[] = {0.8f, 0.8f, 0.8f, 1.0f}; // Diffuse light
+GLfloat lightSpecular[] = {1.0f, 1.0f, 1.0f, 1.0f}; // Specular light
+
 int server_socket;
 std::string nick;
 std::string server_ip;
@@ -33,13 +38,27 @@ time_t last_pong_time = 0;
 
 struct Box {
     int x, y, z;
-    GLubyte r, g, b;
+    GLubyte r, g, b; 
 
     Box() : x(0), y(0), z(0), r(1.0f), g(1.0f), b(1.0f) {}
     Box(int x, int y, int z, GLubyte r, GLubyte g, GLubyte b)
         : x(x), y(y), z(z), r(r), g(g), b(b) {}
 
+    // When digged, drop the color 
+    void Dig()
+    {
+       
+       r/=2;
+       g/=2;
+       b/=2;
+       if(r<0)r=0;
+       if(g<0)g=0;
+       if(b<0)b=0;
 
+    }
+    void Tick()
+    { 
+    }
     bool is_face_visible(const std::unordered_map<std::string, Box>& boxes, int dx, int dy, int dz) const {
         std::string key = std::to_string(x + dx) + "_" + std::to_string(y + dy) + "_" + std::to_string(z + dz);
         return boxes.find(key) == boxes.end();
@@ -72,36 +91,46 @@ int lastMouseX, lastMouseY;
 bool rotating = false;
 
 void draw_face(float x, float y, float z, GLubyte r, GLubyte g, GLubyte b, const std::string& face) {
- 
+    
     glBegin(GL_QUADS);
+
     if (face == "front") {
+        glNormal3f(0.0f, 0.0f, 1.0f);  // Normal pointing out of the screen
         glVertex3f(x - 0.25f, y - 0.25f, z + 0.25f);  // Bottom-left
         glVertex3f(x + 0.25f, y - 0.25f, z + 0.25f);  // Bottom-right
         glVertex3f(x + 0.25f, y + 0.25f, z + 0.25f);  // Top-right
         glVertex3f(x - 0.25f, y + 0.25f, z + 0.25f);  // Top-left
+
     } else if (face == "back") {
+        glNormal3f(0.0f, 0.0f, -1.0f);  // Normal pointing into the screen
         glVertex3f(x - 0.25f, y - 0.25f, z - 0.25f);  // Bottom-left
         glVertex3f(x - 0.25f, y + 0.25f, z - 0.25f);  // Top-left
         glVertex3f(x + 0.25f, y + 0.25f, z - 0.25f);  // Top-right
         glVertex3f(x + 0.25f, y - 0.25f, z - 0.25f);  // Bottom-right
+
     } else if (face == "left") {
+        glNormal3f(-1.0f, 0.0f, 0.0f);  // Normal pointing to the left
         glVertex3f(x - 0.25f, y - 0.25f, z + 0.25f);  // Bottom-left
         glVertex3f(x - 0.25f, y + 0.25f, z + 0.25f);  // Top-left
         glVertex3f(x - 0.25f, y + 0.25f, z - 0.25f);  // Top-right
         glVertex3f(x - 0.25f, y - 0.25f, z - 0.25f);  // Bottom-right
+
     } else if (face == "right") {
+        glNormal3f(1.0f, 0.0f, 0.0f);  // Normal pointing to the right
         glVertex3f(x + 0.25f, y - 0.25f, z - 0.25f);  // Bottom-left
         glVertex3f(x + 0.25f, y + 0.25f, z - 0.25f);  // Top-left
         glVertex3f(x + 0.25f, y + 0.25f, z + 0.25f);  // Top-right
         glVertex3f(x + 0.25f, y - 0.25f, z + 0.25f);  // Bottom-right
 
     } else if (face == "top") {
+        glNormal3f(0.0f, 1.0f, 0.0f);  // Normal pointing up
         glVertex3f(x - 0.25f, y + 0.25f, z - 0.25f);  // Bottom-left
         glVertex3f(x - 0.25f, y + 0.25f, z + 0.25f);  // Top-left
         glVertex3f(x + 0.25f, y + 0.25f, z + 0.25f);  // Top-right
         glVertex3f(x + 0.25f, y + 0.25f, z - 0.25f);  // Bottom-right
 
     } else if (face == "bottom") {
+        glNormal3f(0.0f, -1.0f, 0.0f);  // Normal pointing down
         glVertex3f(x - 0.25f, y - 0.25f, z - 0.25f);  // Bottom-left
         glVertex3f(x + 0.25f, y - 0.25f, z - 0.25f);  // Bottom-right
         glVertex3f(x + 0.25f, y - 0.25f, z + 0.25f);  // Top-right
@@ -110,6 +139,7 @@ void draw_face(float x, float y, float z, GLubyte r, GLubyte g, GLubyte b, const
 
     glEnd();
 }
+
 void draw_box(const Box& box) {
     float x = box.x * BLOCK_SCALE;
     float y = box.y * BLOCK_SCALE;
@@ -190,6 +220,18 @@ void parse_message(const std::string& message) {
         iss >> strPlayer >> score;
         mPlayers[strPlayer]=score;
     }
+    else if (command == "LEAVE")
+    {
+        std::string strNick;
+        iss >> strNick;
+        mPlayers.erase(strNick);
+    }
+    else if (command == "DIG") {
+        int x, y, z;
+        iss >> x >> y >> z;
+        std::string key = std::to_string(x) + "_" + std::to_string(y) + "_" + std::to_string(z);
+        boxes[key].Dig();
+    }
 }
 
 void receive_messages() {
@@ -231,115 +273,156 @@ void RenderText(float x, float y, const std::string& text) {
         glutBitmapCharacter(GLUT_BITMAP_9_BY_15, c);
     }
 }
-void pick(int x, int y) {         
-     
 
+void pick(int x, int y) {
     GLuint selectBuf[PICK_BUFFER_SIZE];
     GLint hits;
     GLint viewport[4];
-    GLubyte pixelColor[3];
 
-    // Setup picking mode
+    // Setup selection buffer and enter selection mode
     glGetIntegerv(GL_VIEWPORT, viewport);
     glSelectBuffer(PICK_BUFFER_SIZE, selectBuf);
     glRenderMode(GL_SELECT);
+
+    // Initialize the name stack
     glInitNames();
     glPushName(0);
-    glClearColor(0,0,0,1);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
+
+    // Set up the projection matrix for picking
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
+
+    // Create a small picking region near the cursor
+    gluPickMatrix((GLdouble)x, (GLdouble)(viewport[3] - y), 5.0, 5.0, viewport);
     gluPerspective(45.0, (double)WIDTH / HEIGHT, 1.0, 100.0);
 
+    // Set up the modelview matrix
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     camera.apply();
-
+    
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+    
+    // Render each box with a unique name
     for (const auto& pair : boxes) {
+        const std::string& key = pair.first;
         const Box& box = pair.second;
-        draw_box(box);
+
+        // Convert the key (e.g., "1_2_3") into a name (unique integer)
+        GLuint name = std::hash<std::string>{}(key);
+        glLoadName(name);  // Assign the name to the current object
+
+        draw_box(box);  // Render the box
     }
+    glDisable(GL_CULL_FACE);
+    glDisable(GL_DEPTH_TEST);
  
+    // Get the hits and return to render mode
     hits = glRenderMode(GL_RENDER);
 
+    // Process hits
     if (hits > 0) {
-        glReadPixels(x, viewport[3] - y - 1, 1, 1, GL_RGB, GL_UNSIGNED_BYTE, pixelColor);
-        
-        // Interpret the pixel color
-        auto it = std::find_if(boxes.begin(), boxes.end(), [pixelColor](const auto& pair) {
-            const Box& box = pair.second;
-            return (box.r  == pixelColor[0] && box.g  == pixelColor[1] && box.b  == pixelColor[2]);
-        });
+        GLuint* ptr = selectBuf;
+        GLuint closestHit = ptr[3]; // The name of the closest hit object
 
-        if (it != boxes.end()) {
-            const Box& box = it->second;
-            std::string dig_command = "DIG " + std::to_string(box.x) + " " + std::to_string(box.y) + " " + std::to_string(box.z) + "\n";
-            send(server_socket, dig_command.c_str(), dig_command.size(), 0);
-            printf("Sent command: %s\n", dig_command.data());
+        // Convert closestHit back to the box key
+        for (const auto& pair : boxes) {
+            const std::string& key = pair.first;
+            GLuint name = std::hash<std::string>{}(key);
+            if (name == closestHit) {
+                // We found the box that was clicked
+                const Box& box = pair.second;
+                std::string dig_command = "DIG " + std::to_string(box.x) + " " + std::to_string(box.y) + " " + std::to_string(box.z) + "\n";
+                send(server_socket, dig_command.c_str(), dig_command.size(), 0);
+                break;
+            }
         }
     }
-
 }
 
 
 
+
+ 
 void display() { 
 
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-    glFrontFace(GL_CCW);  
-
-    //glEnable(GL_FOG); <<< Breaks picking
-    
-    glFogfv(GL_FOG_COLOR, fogColor);
-    glFogi(GL_FOG_MODE, GL_LINEAR);
-    glFogf(GL_FOG_START, 0.0f);
-    glFogf(GL_FOG_END, 50.0f);
-    glHint(GL_FOG_HINT, GL_NICEST);
-
-
+     
     glClearColor(fogColor[0],fogColor[1],fogColor[2],fogColor[3]);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, WIDTH, 0, HEIGHT);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glColor3f(1.0, 1.0, 1.0);
-    RenderText(10, HEIGHT - 20, "Blocks: " + std::to_string(boxes.size()));
-
-    std::string strScores = "";
-    for (const auto& pair : mPlayers) { 
-    strScores += pair.first+":"+std::to_string(pair.second)+" ";
-}
-
-    RenderText(10, 20, strScores);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+    glLoadIdentity();    
     gluPerspective(45.0, (double)WIDTH / HEIGHT, 1.0, 200.0);
 
+
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
     camera.apply();
+
+    
+    glEnable(GL_FOG);   
+    glEnable(GL_COLOR_MATERIAL);    
+    glEnable(GL_LIGHTING); 
+    glEnable(GL_LIGHT0);  
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+ 
 
     for (const auto& pair : boxes) {
         const Box& box = pair.second;
         draw_box(box);
     }
 
-    glutSwapBuffers();
-
     glDisable(GL_CULL_FACE);
     glDisable(GL_DEPTH_TEST);
-    glDisable(GL_FOG);
+    glDisable(GL_LIGHT0);
+    glDisable(GL_LIGHTING);    
+    glDisable(GL_COLOR_MATERIAL);
+    glDisable(GL_FOG); 
+
+
+    // Switch to orthographic projection
+
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    gluOrtho2D(0, WIDTH, 0, HEIGHT); 
+
+    // Switch to modelview matrix for rendering text
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+ // Draw the black bar at the top of the screen
+    glColor3f(0.0f, 0.0f, 0.0f);  // Set color to black
+    glBegin(GL_QUADS);
+    glVertex2f(0, HEIGHT - 32);  // Bottom-left corner of the bar
+    glVertex2f(WIDTH, HEIGHT - 32);  // Bottom-right corner of the bar
+    glVertex2f(WIDTH, HEIGHT);  // Top-right corner of the bar
+    glVertex2f(0, HEIGHT);  // Top-left corner of the bar
+    glEnd();
+
+ // Draw the black bar at the bottom of the screen
+    glColor3f(0.0f, 0.0f, 0.0f);  // Set color to black
+    glBegin(GL_QUADS);
+    glVertex2f(0,  32);  // Bottom-left corner of the bar
+    glVertex2f(WIDTH,  32);  // Bottom-right corner of the bar
+    glVertex2f(WIDTH, 0);  // Top-right corner of the bar
+    glVertex2f(0, 0);  // Top-left corner of the bar
+    glEnd();
+    
+
+    glColor3f(1.0, 1.0, 1.0);
+    RenderText(10, HEIGHT - 20, "Blocks: " + std::to_string(boxes.size()));
+    std::string strScores = "";
+    for (const auto& pair : mPlayers) { 
+        strScores += pair.first+":"+std::to_string(pair.second)+" ";
+    }
+    RenderText(10, 16, strScores);
+
+
+    glutSwapBuffers();
 
 }
  
@@ -361,8 +444,9 @@ void mouse(int button, int state, int x, int y) {
         } else {
             rotating = false;
         }
-    } else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+    } else if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {               
         pick(x, y);
+        glutPostRedisplay();  
     } else if (button == 3) {
         camera.zoom(-1.0f);
         glutPostRedisplay();
@@ -392,6 +476,13 @@ void idle() {
         last_pong_time = current_time;
     }
 
+
+    // Tick the boxes
+    for (auto& pair : boxes) {
+        Box& box = pair.second;
+        box.Tick(); 
+    }
+ 
     glutPostRedisplay();
 }
 int main(int argc, char** argv) {
@@ -420,6 +511,23 @@ int main(int argc, char** argv) {
     glutInitWindowSize(WIDTH, HEIGHT);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Multiplayer Digger");
+
+    glFogfv(GL_FOG_COLOR, fogColor);
+    glFogi(GL_FOG_MODE, GL_LINEAR);
+    glFogf(GL_FOG_START, 0.0f);
+    glFogf(GL_FOG_END, 50.0f);
+    glHint(GL_FOG_HINT, GL_NICEST);
+    
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, lightAmbient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightDiffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular);
+
+    glDepthFunc(GL_LESS);    
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);  
+
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
